469a0612bcf2737f7919ec4f4d1495c9f7035593
ed k <you@example.com>
2026-04-15 23:46:34 +0300

    Fix imports path issues

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,23 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const connectionString =
+      `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT ?? '5432'}/${process.env.DB_DATABASE}`;
+    const configuredUrl = process.env.DATABASE_URL;
+
+    super({
+      adapter: new PrismaPg({
+        connectionString: configuredUrl?.startsWith('postgres')
+          ? configuredUrl
+          : connectionString,
+      }),
+    });
+  }
+
   // Logic to connect when the NestJS app starts
   async onModuleInit() {
     await this.$connect();
